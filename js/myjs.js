7b79aa0943cf490fe93c768cf10ee98baf858a8b
Vivek Singh <vivek@happay.in>
2016-05-30 04:14:18 +0530

    done till work, edu and skills

--- js/myjs.js
+++ js/myjs.js
@@ -14,4 +14,31 @@ $(document).ready(function() {
       $('.exp-detail-1').addClass('hidden');
       $('.exp-detail-2').removeClass('hidden');
     });
+
+    console.log("Hello");
+    $('.next').click(function() {
+        var curEdu = $('.active-edu');
+        var nextEdu = curEdu.next();
+        if(nextEdu.length === 0)
+        {
+            nextEdu = $('.edu').first();
+        }
+        curEdu.removeClass('active-edu');
+        curEdu.fadeOut();
+        nextEdu.addClass('active-edu');
+        nextEdu.fadeIn();
+    })
+
+    $('.prev').click(function() {
+        var curEdu = $('.active-edu');
+        var prevEdu = curEdu.prev();
+        if(prevEdu.length === 0)
+        {
+            prevEdu = $('.edu').last();
+        }
+        curEdu.removeClass('active-edu');
+        curEdu.fadeOut();
+        prevEdu.addClass('active-edu');
+        prevEdu.fadeIn();
+    })
 });
